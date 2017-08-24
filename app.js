458ac1348876c2f60bed0e23654d66fb8ae48467
sramayanam <sreekanth_bhr@yahoo.com>
2017-08-24 08:10:43 -0500

    changed www and app to catch 500

--- app.js
+++ app.js
@@ -30,6 +30,14 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 /*var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
 });
