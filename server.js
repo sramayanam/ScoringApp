1c27bf28f04d838c2cb054706fac1949646e4015
sramayanam <sreekanth_bhr@yahoo.com>
2017-08-29 20:32:43 -0500

    route changed to route

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var bodyParser = require('body-parser');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');    
 app.use('/', index);
-app.use('/api', api);
+app.use('/', api);
 //app.set('port', process.env.PORT || 3000);
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 //middle ware
